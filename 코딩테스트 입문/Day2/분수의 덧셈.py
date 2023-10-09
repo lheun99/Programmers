ff1c09d5180d3664a345532958eaceff03e8e7a0
Lee Ha-eun <lheun99@gmail.com>
2023-10-09 16:00:40 +0900

    Day2 분수의 덧셈_math.gcd

--- 코딩테스트 입문/Day2/분수의 덧셈.py
+++ 코딩테스트 입문/Day2/분수의 덧셈.py
@@ -1,3 +1,4 @@
+import math
 from fractions import Fraction
 
 
@@ -6,3 +7,16 @@ def solution(denum1, num1, denum2, num2):
     answer = [num.numerator, num.denominator]
 
     return answer
+
+
+def solution2(numer1, denom1, numer2, denom2):
+    num1 = denom1 * denom2
+    num2 = (numer1 * denom2) + (numer2 * denom1)
+
+    gcd = math.gcd(num1, num2)
+
+    print(num2//gcd, num1//gcd)
+
+
+solution2(1, 2, 3, 4)
+solution2(9, 2, 1, 3)
