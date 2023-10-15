a56a790b9f7fe2c0f4e320eb08b9b1a84440885a
Lee Ha-eun <lheun99@gmail.com>
2023-10-15 19:07:18 +0900

    Level1 python 달리기 경주

---
+++ LEVEL1/PYTHON/달리기 경주.py
@@ -0,0 +1,26 @@
+def solution(players, callings):
+    rank = {}
+    for i, player in enumerate(players):
+        rank[i] = player
+
+    name = {}
+    for i, player in enumerate(players):
+        name[player] = i
+
+    for call in callings:
+        # call: 불린 선수 name
+        c_rank = name[call]  # 불린 선수 rank
+        c_f_rank = c_rank - 1  # 앞 선수 rank
+        c_f_name = rank[c_f_rank]  # 앞 선수 name
+
+        rank[c_rank] = c_f_name
+        rank[c_f_rank] = call
+
+        name[call] = c_f_rank
+        name[c_f_name] = c_rank
+
+    return list(rank.values())
+
+
+print(solution(["mumu", "soe", "poe", "kai", "mine"],
+      ["kai", "kai", "mine", "mine"]))
